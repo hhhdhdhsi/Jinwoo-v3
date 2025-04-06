c4e5f074ed22dc06e14ea68c15d6ef592a62de46
hhhdhdhsi <nbot86000@gmail.com>
2025-04-06 20:53:41 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -23,8 +23,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "👾 ᴍᴀʟᴠɪɴ ᴋɪɴɢ 👾",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "94720797915",
+  OWNER_NAME: process.env.OWNER_NAME || "KING GANESHA-MD",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "94741252876",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
